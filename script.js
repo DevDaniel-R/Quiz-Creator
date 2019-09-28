034625bdbd793c14ec9505464a119cb6c964a6d8
Daniel Rodriguez <drodriguez661@gmail.com>
2019-09-28 07:49:27 -0500

    tested module pattern. 3(quizController, UIController, and controller) we have access to quizController and UIController through controller. we tested that out by adding from both modules and outputting to console in controller

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var quizController = (function () {
+/* QUIZ CONTROLLER we will work on data which will appear in user interface and will be managed through UICONTROLLER QCONTROLLER UICONTROLLER will work independantly, CONTROLLER will let UICONTROLLER to recieve data from QUIZCONTROLLER*/
+
+var quizController = (function() {
   var private = 10;
 
   var privateFn = function(a) {
@@ -12,3 +14,21 @@ var quizController = (function () {
   };
 
 }) ();
+
+
+var UIController = (function() {
+
+  var num1 = 30;
+
+  return {
+    sum: function(num2) {
+      return num1 + num2;
+    }
+  }
+})();
+
+var controller  = (function(quizCtrl, UICtrl){
+
+  console.log(UICtrl.sum(100) + quizCtrl.publicMethod());
+
+})(quizController, UIController);
