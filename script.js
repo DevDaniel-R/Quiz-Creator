e00fd97fe1c5cf28c782f5c2b8533e1229ae964f
Daniel Rodriguez <drodriguez661@gmail.com>
2019-09-28 07:31:41 -0500

    starting with javascript will be using a module pattern for application

---
+++ script.js
@@ -0,0 +1,14 @@
+var quizController = (function () {
+  var private = 10;
+
+  var privateFn = function(a) {
+    return a + private;
+  }
+
+  return {
+    publicMethod: function(){
+      return privateFn(20);
+    }
+  };
+
+}) ();
